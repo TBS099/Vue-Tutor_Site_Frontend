9612190e8b36ed581d9ab67f3af28fa5777a8050
Tanush Bikram Shah <109902776+TBS099@users.noreply.github.com>
2025-11-08 11:07:22 +0000

    created navbar and basic app files

---
+++ src/App/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+    <SiteNavbar :cartCount="totalCartQuantity" @navigate="handleNavigation" />
+  </div>
+</template>
+
+<script>
+import SiteNavbar from '../Components/Navbar/Navbar.vue'
+
+</script>
---
+++ src/Components/Navbar/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar">
+    <h3 class="logo">ClassCart</h3>
+    <ul>
+      <li><button class="nav-link" @click="$emit('navigate', 'lessons')">Lessons</button></li>
+      <li>
+        <button class="nav-link" @click="$emit('navigate', 'cart')">Cart ({{ cartCount }})</button>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import './Navbar.css'
+
+export default {
+  name: 'SiteNavbar',
+  props: {
+    cartCount: { type: Number, default: 0 },
+  },
+}
+</script>
